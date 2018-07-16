22d7e71f45058a3828bd7e6bb7434a09d88de5af
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2018-07-16 11:33:28 +0100

    (#6) Add store persistance to the localStorage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 import createRoutes from './routes';
 import App from './App';
 
-const store = configureStore();
+import 'semantic-ui-css/semantic.min.css';
+
+const persistedState = loadState();
+
+const store = configureStore(persistedState);
 const routes = createRoutes();
+
+store.subscribe( throttle( () => {
+    saveState({
+        user: store.getState().user
+    });
+}, 1000));
 
 ReactDOM.render(
     <Provider store={store}>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,9 +3,10 @@ import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default function configureStore() {
+export default function configureStore(persistedState) {
     return createStore(
         rootReducer,
+        persistedState,
         composeEnhancers(applyMiddleware(thunk))
     );
 }
